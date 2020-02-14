3bcacc566a680bdb6b908e8ce6abede03e23a110
Trejon <50248674+Trejon@users.noreply.github.com>
2020-02-14 08:50:55 -0700

    adding router

---
+++ bookListerFrontend/src/router.js
@@ -0,0 +1,23 @@
+class Router {
+
+  constructor(kvpairs){
+    this.routes = kvpairs
+  }
+
+  set rootPage(rootPageKey){
+    this.rootPage = this.routes[rootPageKey]
+  }
+
+  render(page){
+      this.routes[page].render()
+    }
+
+  assignCallback(callback){
+      for(let route in this.routes){
+        this.routes[route].redirect = callback
+      }
+    }
+
+
+
+}
