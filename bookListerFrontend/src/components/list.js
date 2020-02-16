e0c480a3d863adb6346f8b16fb5e5d188ed21057
Trejon <50248674+Trejon@users.noreply.github.com>
2020-02-16 14:33:36 -0700

    created show for lists

--- bookListerFrontend/src/components/list.js
+++ bookListerFrontend/src/components/list.js
@@ -1,15 +1,48 @@
 class List{
-  constructor(list){
-    const { id, name, description } = list
-      this.id = id
-      this.name = name
-      this.description = description
-  }
+    constructor(list){
+      const { id, name, description } = list
+        this.id = id
+        this.name = name
+        this.description = description
+      }
 
-  get liAndLinkHTML() {
-      return `<li><a href="#" data-id=${this.id}>${this.name} - ${this.description}</a></li>`
-  }
+    static formHTML(list) {
+          return(`
+            <form id="${list ? 'edit' : 'new'}-list-form">
+                ${list ? `<input type='hidden' value="` + list.id + `">` : ''}
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" placeholder="Name" value=${list ? list.name : ''} required >
+                  </div>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="description">Description</label>
+                     <textarea class="form-control" id="description" rows="3">${list ? list.description : ''}</textarea>
+                  </div>
+                </div>
+              <button type="submit" class="btn btn-primary">${list ? 'Update' : 'Create'}</button>
+            </form>
+            `)
+      }
 
+      get formHTML() {
+          return List.formHTML(this)
+        }
+
+
+      get showHTML() {
+        return (`
+            <h2>${this.name}</h2>
+            <p>Description:\n${this.description ? this.description : "None"}</p>
+            <button data-id=${this.id} id="edit-list">Edit</button>
+          `)
+        }
+
+      get liAndLinkHTML() {
+        return `<li><a href="#" data-id=${this.id}>${this.name} - ${this.description}</a></li>`
+      }
 
 
 }
